1d0226bc647663f91192d47cb35715676e0a30c2
bhanu <kumarbhanu25@gmail.com>
2024-08-13 18:20:35 +0530

    ui imporvemnets

--- frontend/src/components/MarkdownEditor.jsx
+++ frontend/src/components/MarkdownEditor.jsx
@@ -1,43 +1,47 @@
-
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { convertURl } from '../constants';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { convertURl } from "../constants";
 
 const MarkdownEditor = () => {
-  const [markdown, setMarkdown] = useState('');
-  const [html, setHtml] = useState('');
+  const [markdown, setMarkdown] = useState("");
+  const [html, setHtml] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const convertFunc = async () => {
-
+      setLoading(true);
       try {
         const response = await axios.post(convertURl, { markdown });
         setHtml(response?.data?.html);
+        setLoading(false);
       } catch (error) {
-        console.error('Error converting markdown:', error);
+        console.error("Error converting markdown:", error);
+        setError("something went wrong");
+        setLoading(false);
       }
     };
 
     convertFunc();
   }, [markdown]);
-
+  if (loading) {
+    return <div>Loading ....</div>;
+  }
   return (
-    <div className="container">
-
-      <textarea
-        value={markdown}
-        onChange={(e) => setMarkdown(e.target.value)}
-        placeholder="Enter markdown "
-      />
-   <div>
-    <h2>Output panel</h2>
-   <div
-        className="htmlPanel"
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
-   </div>
-    </div>
+    <>
+      <p className="error">{error}</p>
+      <div className="container">
+        <textarea
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+          placeholder="Enter markdown "
+        />
+        <div className="htmlPanel">
+          <h2>Output panel</h2>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+        </div>
+      </div>
+    </>
   );
 };
 
